11826dc3573f42409254fb347730e08224864661
krisna ahroid <krisna@clappingape.com>
2018-11-13 22:50:46 +0700

    get data

---
+++ playground/mongodb-find.js
@@ -0,0 +1,22 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+  if(err){
+    return console.log('Unable to connect to mongodb');
+  }
+  console.log('connected to mongodb');
+  //
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Data count is : ${count}`);
+  // }, (err) => {
+  //   console.log(err);
+  // });
+
+  db.collection('Users').find({name: 'Krisna'}).toArray().then((data) => {
+    console.log(data);
+  })
+
+  db.close();
+});
